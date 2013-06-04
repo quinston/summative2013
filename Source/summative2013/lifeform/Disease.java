ee07fb96d80fa80784bf8d63aa7b5f5eb3e32df9
Stephen Wen <stephen.c.wen@gmail.com>
2013-06-04 18:41:24 -0400

    added disease and memory classes
    Currently these classes do nothing. Also set the return type of Act to
    void so it isn't a constructor

---
+++ Source/summative2013/lifeform/Disease.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package summative2013.lifeform;
+
+/**
+ *
+ * @author Bobby Chiang, Quincy Lam, Stephen Wen
+ */
+public class Disease {
+    public void takeToll(){
+        
+    }
+}
